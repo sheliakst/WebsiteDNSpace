edb408ffca2303d70f4162055c9555a194d0ff8e
sheliakst <stamcj@hotmail.com>
2019-02-24 22:00:55 +0200

    aos

--- js/index.js
+++ js/index.js
@@ -1,27 +1,18 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        let scroll = $(this).scrollTop();
-        $("#moon").css(({ WebkitTransform: 'rotate(' + scroll/30 + 'deg)'}))
+$(document).ready(function() {
+  AOS.init();
 
-    });
-$(".navicon").on("click",function(){
-    
-  
-    $("#mySidenav a").css("opacity","1");
-    $("#mySidenav").css("width","100vw");
+  $(window).scroll(function() {
+    let scroll = $(this).scrollTop();
+    $("#moon").css({ WebkitTransform: "rotate(" + scroll / 30 + "deg)" });
+  });
+  $(".navicon").on("click", function() {
+    $("#mySidenav a").css("opacity", "1");
+    $("#mySidenav").css("width", "100vw");
+  });
 
+  $(".sidenav").on("click", function() {
+    $("#mySidenav a").css("opacity", "0");
+
+    $("#mySidenav").css("width", "0");
+  });
 });
-
-$(".sidenav").on("click",function(){
-
-
-    $("#mySidenav a").css("opacity","0");
-
-    $("#mySidenav").css("width","0");
-
-});
-
-
-});
-
-
